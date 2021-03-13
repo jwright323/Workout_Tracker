60797b5dab0565493768971413ec7f01c5dc0089
jwright323 <54717725+jwright323@users.noreply.github.com>
2021-03-12 21:57:41 -0500

    Add files via upload

---
+++ routes/api-routes.js
@@ -0,0 +1,40 @@
+const db = require("../models");
+
+module.exports = function(app) {
+    // Pull up info for the workouts page
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).then(dbwork => {
+            res.json(dbwork);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    })
+    // Pull up info for the range page
+    app.get("/api/workouts/range", ({}, res) => {
+        db.Workout.find({}).then((dbwork) => {
+            res.json(dbwork);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+    });
+    // Submit new completed workouts
+    app.post("/api/workouts/", (req, res) => {
+        db.Workout.create(req.body).then((dbwork) => {
+            res.json(dbwork);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+    });
+      // Update workouts by _id value and update the exercise body
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+            { _id: req.params.id }, 
+            { $push: {exercises: req.body }}
+        ).then((dbwork) => {
+            res.json(dbwork);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+    });
+};
